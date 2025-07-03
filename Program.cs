9967edf45dd2b640696a00fc7ded1ba4f01a96e0
Amanda Ambrosio <amandaambrosio295@gmail.com>
2025-07-03 20:31:13 -0300

    resolução projeto POO .NET

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "85998074839", modelo: "Nokia Tijolão", imei: "123", memoria: 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Temple Run");
+
+        Console.WriteLine("Testando smartphone iPhone:");
+        Smartphone iphone = new Iphone(numero: "11998345670", modelo: "Iphone 8", imei: "999", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Safari");
+    }
+}
